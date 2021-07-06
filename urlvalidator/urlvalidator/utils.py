026a31719a35ab38e20ab3cd9455d60b388c8686
biswajeet <biswaj.padhi@gmail.com>
2021-07-06 11:23:10 +0530

    Making common component

--- urlvalidator/urlvalidator/utils.py
+++ urlvalidator/urlvalidator/utils.py
@@ -3,18 +3,29 @@ import requests
 import json
 
 
+def get_urlscan_ind(verdict_api):
+    verdict_api_response = requests.get(verdict_api)
+    verdict_dict = (json.loads(verdict_api_response.text))['verdicts']
+    malicious_ind = False
+    if verdict_dict['overall']['malicious'] or verdict_dict['engines']['maliciousTotal'] or verdict_dict['community']['votesMalicious']:
+        malicious_ind = True
+    return malicious_ind
+
+
 def urlscan(Ip):
     data = {
         "url": Ip,
-        "visibility": "public",
+        "visibility": "private",
         "tags": "malicious"
     }
     headers = {
         'API-Key': 'cbab9d23-7028-4d33-8242-7aebc4019c66',
         'Content-Type': 'application/json'
     }
+
     response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data))
     return response.json(), response.status_code
+
 
 
 def virusscanip(Ip):
--- urlvalidator/urlvalidator/views.py
+++ urlvalidator/urlvalidator/views.py
@@ -1,42 +1,42 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import re
-import requests
-import json
 from .utils import *
+
+
+def get_ip_report(url_string):
+    if validate_ip(url_string):
+        final_response = dict()
+        final_response['urlscan'] = list(urlscan(url_string))
+        virus_scan, status_code, malicious = virusscanip(url_string)
+        final_response['virustotal'] = [virus_scan, status_code]
+        final_response['malicious'] = malicious
+        return Response(final_response, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+def get_domain_report(url_string):
+    if validate_domain(url_string):
+        final_response = dict()
+        final_response['urlscan'] = list(urlscan(url_string))
+        virus_scan, status_code, malicious = virusscandomain(url_string)
+        final_response['virustotal'] = [virus_scan, status_code]
+        final_response['malicious'] = malicious
+        return Response(final_response, status=status.HTTP_200_OK)
+
+    return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 class UrlValidateippath(APIView):
 
     def get(self, request, url_string):
-        if validate_ip(url_string):
-            final_response = dict()
-            final_response['urlscan'] = list(urlscan(url_string))
-            virus_scan, status_code, malicious = virusscanip(url_string)
-            final_response['virustotal'] = [virus_scan, status_code]
-            final_response['malicious'] = malicious
-            return Response(final_response, status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def post(self, request):
-        ip = request.body.get('ip')
-        print(ip)
-        return Response(status=status.HTTP_200_OK)
+        return get_ip_report(url_string)
 
 
 class UrlValidatedomainpath(APIView):
 
     def get(self, request, url_string):
-        if validate_domain(url_string):
-            final_response = dict()
-            final_response['urlscan'] = list(urlscan(url_string))
-            virus_scan, status_code, malicious = virusscandomain(url_string)
-            final_response['virustotal'] = [virus_scan, status_code]
-            final_response['malicious'] = malicious
-            return Response(final_response, status=status.HTTP_200_OK)
-
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return get_domain_report(url_string)
 
 
 class UrlValidatePost(APIView):
@@ -44,25 +44,11 @@ class UrlValidatePost(APIView):
         request = json.loads(request.body)
         if 'ip' in request.keys():
             url_string = request['ip']
-            if validate_ip(url_string):
-                final_response = dict()
-                final_response['urlscan'] = list(urlscan(url_string))
-                virus_scan, status_code, malicious = virusscanip(url_string)
-                final_response['virustotal'] = [virus_scan, status_code]
-                final_response['malicious'] = malicious
-                return Response(final_response, status=status.HTTP_200_OK)
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return get_ip_report(url_string)
+
         elif 'domain' in request.keys():
             url_string = request['domain']
-            if validate_domain(url_string):
-                final_response = dict()
-                final_response['urlscan'] = list(urlscan(url_string))
-                virus_scan, status_code, malicious = virusscandomain(url_string)
-                final_response['virustotal'] = [virus_scan, status_code]
-                final_response['malicious'] = malicious
-                return Response(final_response, status=status.HTTP_200_OK)
-
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return get_domain_report(url_string)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -76,21 +62,6 @@ class UrlValidateParams(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         if type == 'domain':
-            if validate_domain(url_string):
-                final_response = dict()
-                final_response['urlscan'] = list(urlscan(url_string))
-                virus_scan, status_code, malicious = virusscandomain(url_string)
-                final_response['virustotal'] = [virus_scan, status_code]
-                final_response['malicious'] = malicious
-                return Response(final_response, status=status.HTTP_200_OK)
-
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return get_domain_report(url_string)
         else:
-            if validate_ip(url_string):
-                final_response = dict()
-                final_response['urlscan'] = list(urlscan(url_string))
-                virus_scan, status_code, malicious = virusscanip(url_string)
-                final_response['virustotal'] = [virus_scan, status_code]
-                final_response['malicious'] = malicious
-                return Response(final_response, status=status.HTTP_200_OK)
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return get_ip_report(url_string)
